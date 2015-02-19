28fce225a9218150b08818db643199694e467d8d
phitchcock <phitchcock@me.com>
2015-02-18 16:32:42 -0800

    added combinations and is_prime methods, still working on

--- challenge.rb
+++ challenge.rb
@@ -23,12 +23,36 @@ puts factorial(10)
 
 def uniques(data)
   result = []
-  uniq_array = []
+  #uniq_array = []
+
   #result = [data]
   #uniq_array = []
   data.each do |item|
-    result << item
+    if item == item
+      result << item
+    end
+    result.uniq
   end
 end
 
 puts uniques([1,2,3,3])
+
+def combinations(one, two)
+  result1 = [one]
+  result2 = [two]
+  result = result1.concat(result2)
+  puts result
+end
+
+combinations(["one", "two"], ["three", "four"])
+
+def is_prime(num)
+  for d in 2..(num - 1)
+   if (num % d) == 0
+    return false
+   end
+  end
+  true
+end
+
+is_prime(1)
